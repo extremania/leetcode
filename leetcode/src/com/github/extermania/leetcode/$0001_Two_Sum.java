2acfdf327c8f8d19e321d0ca5ec07e823f4eff13
Administrator <Administrator@PC201404021135>
2019-02-17 10:16:24 +0800

    first submission

---
+++ leetcode/src/com/github/extermania/leetcode/$0001_Two_Sum.java
@@ -0,0 +1,18 @@
+package com.github.extermania.leetcode;
+
+public class $0001_Two_Sum {
+	class Solution {
+	    public int[] twoSum(int[] nums, int target) {
+	        int a = 0;
+	        int b = 0;
+	        for(int i=0; i<nums.length; i++){
+	            for(int j=i+1; j<nums.length; j++){
+	            a = nums[i];
+	            b = nums[j];
+	            if(a+b==target) return new int[] {i, j};
+	            }
+	        }
+	        return new int[] {0,0};
+	    }
+	}
+}
